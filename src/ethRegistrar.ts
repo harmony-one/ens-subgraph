8f672ba170ee878eafb6b1b07ef01e47f8bf5f4b
polymorpher <aaron@qokka.ai>
2022-11-15 23:19:17 -0800

    fix addresses

--- src/ethRegistrar.ts
+++ src/ethRegistrar.ts
@@ -30,10 +30,10 @@ import {
 import { Account, Domain, NameRegistered, NameRenewed, NameTransferred, Registration } from './types/schema'
 
 // .eth
-// var rootNode:ByteArray = byteArrayFromHex("93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae")
+var rootNode:ByteArray = byteArrayFromHex("93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae")
 
 // .country
-const rootNode:ByteArray = byteArrayFromHex("0xad4be81514036b9f6ff6c5f69394daacc516c82bd6dc4756d7f6ef1b3f9ea717")
+// const rootNode:ByteArray = byteArrayFromHex("0xad4be81514036b9f6ff6c5f69394daacc516c82bd6dc4756d7f6ef1b3f9ea717")
 
 export function handleNameRegistered(event: NameRegisteredEvent): void {
   let account = new Account(event.params.owner.toHex())
